297caeab1dff9c911ec3fb6615fbd989d22fcf93
DarklMoon <92285604+DarklMoon@users.noreply.github.com>
2021-11-06 12:01:00 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,15 +1,21 @@
 import pygame, sys
-#Initialize Pygame
-pygame.init() # เซตค่าเริ่มต้นโมดูลทั้งหมดที่จำเป็นสำหรับ pygame
 
-WIDTH = 600  # เขียนตัวแปรในรูปแบบของค่าคงที่
-HEIGHT = 600 # เก็บค่าที่เป็นตัวเลข ใช้ในการกำหนดขนาดหน้าจอของเกม
-# สร้างหน้าจอสำหรับตัวเกม
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 600
+
+screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
+pygame.display.set_caption( 'TIC TAC TOE')
+
+#define variable
+clicked = False
 
 FPS = 60
 
 def draw_board():
+    BG_COLOR = (102, 178, 255)
+    LIGHT_PURPLE = (51, 153, 255)
     screen.fill( BG_COLOR )
     #Vertical
     pygame.draw.line( screen, LIGHT_PURPLE, (200, 0), (200, 600), 20) #1
@@ -17,15 +23,26 @@ def draw_board():
     #Horizontal
     pygame.draw.line( screen, LIGHT_PURPLE, (0, 200), (600, 200), 20) #1
     pygame.draw.line( screen, LIGHT_PURPLE, (0, 400), (600, 400), 20) #2
-    pygame.display.update()
 
-# สร้าง whileloop เพื่อให้ code รันต่อเนื่อง
+
+
+#mainloop
 def main():
     clock = pygame.time.Clock()
     run = True
     while run:
-        for event in pygame.event.get(): #ใช้ forloop ในการเช็คดูเหตุการณ์ต่างๆที่เกิดขึ้นภายในเกม
-            if event.type == pygame.QUIT: # หากมีการกดปิดเกมให้สั่งปิด
-                sys.exit() #คำสั่งสำหรับกดปุ่มปิด แล้วทำให้เกมปิดได้
         draw_board()
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+                clicked = True
+            if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+                clicked = False
+
+        pygame.display.update()
     pygame.quit()
+
+if __name__ == "__main__":
+    main()
